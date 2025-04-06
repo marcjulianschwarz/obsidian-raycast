9f36c87854e8bafe51b4362ecd2cde38f75dfdb1
marcjulianschwarz <info@marc-julian.de>
2025-04-06 22:17:51 +0200

    do not use pop()

--- src/api/vault/notes/notes.service.ts
+++ src/api/vault/notes/notes.service.ts
@@ -88,7 +88,7 @@ function createObsidianProperties(tags: string[]): string {
     for (let i = 0; i < tags.length - 1; i++) {
       obsidianProperties += '"' + tags[i] + '",';
     }
-    obsidianProperties += '"' + tags.pop() + '"]\n---\n';
+    obsidianProperties += '"' + tags[tags.length - 1] + '"]\n---\n';
   }
 
   return obsidianProperties;
