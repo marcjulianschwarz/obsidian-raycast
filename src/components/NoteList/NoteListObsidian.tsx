899fe11e0047a73e527a55a76c31c15972fa0234
marcjulianschwarz <info@marc-julian.de>
2025-05-04 22:02:38 +0200

    simplify stuff

--- src/components/NoteList/NoteListObsidian.tsx
+++ src/components/NoteList/NoteListObsidian.tsx
@@ -1,11 +1,9 @@
 import { useNotes } from "../../utils/hooks";
 import { SearchArguments } from "../../utils/interfaces";
 import { NoteList } from "./NoteList";
-import { NoteActions, OpenNoteActions } from "../../utils/actions";
-import { Action, Icon } from "@raycast/api";
+import { OpenNoteActions } from "../../utils/actions";
 import { Vault } from "../../api/vault/vault.types";
 import { Note } from "../../api/vault/notes/notes.types";
-import { renewCache } from "../../api/cache/cache.service";
 
 export const NoteListObsidian = function NoteListObsidian(props: {
   vault: Vault;
@@ -15,11 +13,7 @@ export const NoteListObsidian = function NoteListObsidian(props: {
 }) {
   const { showTitle, vault, searchArguments } = props;
 
-  const { notes, loading } = useNotes(vault);
-
-  // const filteredNotes = useMemo(() => {
-  //   return props.bookmarked ? notes.filter((n) => n.bookmarked) : notes;
-  // }, [notes, props.bookmarked]);
+  const { notes, loading } = useNotes(vault, false);
 
   return (
     <NoteList
