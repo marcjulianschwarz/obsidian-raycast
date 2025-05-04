6f1e28b7c7d00f55b784f7b14a2f75f6f77a4ab5
marcjulianschwarz <info@marc-julian.de>
2025-05-04 23:30:50 +0200

    update more components

--- src/components/RandomNote.tsx
+++ src/components/RandomNote.tsx
@@ -1,8 +1,23 @@
+import { Detail } from "@raycast/api";
+import { useMemo } from "react";
 import { Vault } from "../api/vault/vault.types";
-// import { NoteQuickLook } from "./NoteQuickLook";
+import { useNotes } from "../utils/hooks";
+import { NoteQuickLook } from "./NoteQuickLook";
 
 export function RandomNote(props: { vault: Vault; showTitle: boolean }) {
-  // const { vault, showTitle } = props;
-  // const randomNote = notes[Math.floor(Math.random() * notes.length)];
-  // return <NoteQuickLook note={randomNote} vault={vault} showTitle={showTitle} allNotes={notes} />;
+  const { vault, showTitle } = props;
+  const { notes, loading: notesLoading } = useNotes(vault);
+
+  const randomNote = useMemo(() => {
+    if (!notesLoading && notes && notes.length > 0) {
+      return notes[Math.floor(Math.random() * notes.length)];
+    }
+    return undefined;
+  }, [notes, notesLoading]);
+
+  if (notesLoading || !randomNote) {
+    return <Detail isLoading={true} />;
+  }
+
+  return <NoteQuickLook note={randomNote} vault={vault} showTitle={showTitle} />;
 }
